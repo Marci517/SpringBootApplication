4be24f45d2b8d1ace82ffd8c9812b5faa5c0d8fc
Bencze Márton <benczemarton38@gmail.com>
2024-11-06 13:38:12 +0200

    uj kodellenorzok

--- src/main/java/edu/bbte/idde/bmim2214/dataaccess/factory/AbstractDaoFactory.java
+++ src/main/java/edu/bbte/idde/bmim2214/dataaccess/factory/AbstractDaoFactory.java
@@ -5,15 +5,11 @@ import edu.bbte.idde.bmim2214.dataaccess.dao.CarDao;
 public abstract class AbstractDaoFactory {
     private static volatile AbstractDaoFactory instance; // minden szal szamara lathato
 
-    public static AbstractDaoFactory getInstance() {
-
-        synchronized (AbstractDaoFactory.class) { // class lock
-            if (instance == null) {
-                instance = new CarDaoJdbcFactory();
-                //instance = new CarDaoMemoFactory();
-            }
+    public static synchronized AbstractDaoFactory getInstance() {
+        if (instance == null) {
+            instance = new CarDaoJdbcFactory();
+            //instance = new CarDaoMemoFactory();
         }
-
         return instance;
     }
 
